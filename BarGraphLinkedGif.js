08fc4a07e23995ba4d1e7a3e19edcd0dd23623b3
Anwesh43 <anweshthecool0@gmail.com>
2018-07-28 19:41:33 +0530

    adding BGNode class

--- BarGraphLinkedGif.js
+++ BarGraphLinkedGif.js
@@ -1,4 +1,4 @@
-const nodes = 5
+const nodes = 5, w = 300, h = 600
 
 class State {
     constructor() {
@@ -24,5 +24,48 @@ class State {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
         }
+    }
+}
+
+class BGNode {
+    constructor(i) {
+        this.i = i
+        this.state = new State()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new BGNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context) {
+        const gap = (w) / nodes
+        const hGap = h / 2
+        context.save()
+        context.translate(this.i * gap, h/2)
+        context.fillRect(0, 0, gap, h / 2 * (1 - this.state.scale) * (1 - 2 * (this.i % 2)))
+        context.restore()
+    }
+
+    update(stopcb) {
+        this.state.update(stopcb)
+    }
+
+    startUpdating() {
+        this.state.startUpdating()
+    }
+
+    getNext(dir, cb) {
+        var curr = this.next
+        if (dir == -1) {
+            curr = this.dir
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
